b0604ba0a88de38ae356b45a4d101f541472e965
ChaehoonGwak <81948389+ChaehoonGwak@users.noreply.github.com>
2021-11-23 15:43:06 +0900

    Create app.js

---
+++ JavaScript/바닐라_JS로_크롬_앱_만들기/const_and_let/app.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 2;
+let myName = 'Chaehoon'
+
+// var , let, const
+// always const, sometimes let, never var
+// const : 값 변경 불가, let : 변수 선언, var : 구식
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello ' + myName);
+
+myName = 'GwakChaehoon';
+
+console.log('your new name is ' + myName)
