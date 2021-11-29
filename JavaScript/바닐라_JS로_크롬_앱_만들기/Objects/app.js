199688456200ff41841a297089e6d77f59fa3a1a
ChaehoonGwak <81948389+ChaehoonGwak@users.noreply.github.com>
2021-11-29 16:14:48 +0900

    Update app.js

--- JavaScript/바닐라_JS로_크롬_앱_만들기/Objects/app.js
+++ JavaScript/바닐라_JS로_크롬_앱_만들기/Objects/app.js
@@ -4,12 +4,17 @@ const player = {
     fat: true,
 };
 
+// 오브젝트 출력
 console.log(player);
+
+// 오브젝트 원소 출력
 console.log(player.name);
 console.log(player['name']);
 
+// 기존의 값 update
 player.fat = false;
 console.log(player);
 
+// 새로운 값 추가
 player.lastName = 'Gwak';
 console.log(player);
