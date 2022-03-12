fd21074da1e94eb9868f3e6d31137f6451537afd
ChaehoonGwak <81948389+ChaehoonGwak@users.noreply.github.com>
2022-03-12 23:00:55 +0900

    Update 서울에서_김서방_찾기.py

--- Algorithm/서울에서_김서방_찾기.py
+++ Algorithm/서울에서_김서방_찾기.py
@@ -7,6 +7,8 @@ seoul은 길이 1 이상, 1000 이하인 배열입니다.
 seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
 "Kim"은 반드시 seoul 안에 포함되어 있습니다.
 
+
+
 def solution(seoul):
     answer = seoul.index("Kim")
     return "김서방은 " + str(answer) + "에 있다"
